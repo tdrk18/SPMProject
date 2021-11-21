ba8d28b334132cd60d21aa1f341b6ff9ff467aa6
tdrk18 <s18.todoroki@gmail.com>
2021-11-21 16:18:53 +0900

    Add staging project

---
+++ Staging/Staging/StagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StagingApp.swift
+//  Staging
+//
+//  Created by tdrk18 on 2021/11/21.
+//
+
+import SwiftUI
+
+@main
+struct StagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
