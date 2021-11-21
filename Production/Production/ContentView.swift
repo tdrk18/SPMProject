0de08621c539a8ddb194cff5163c3780d4ddb442
tdrk18 <s18.todoroki@gmail.com>
2021-11-21 16:29:07 +0900

    Add production project

---
+++ Production/Production/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Production
+//
+//  Created by tdrk18 on 2021/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Production/Production/ProductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductionApp.swift
+//  Production
+//
+//  Created by tdrk18 on 2021/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ProductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
