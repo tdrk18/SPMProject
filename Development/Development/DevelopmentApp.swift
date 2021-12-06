be9bbc576c90fe5ecef83a29fd03341105aefc01
tdrk18 <tdrk18@gmail.com>
2021-12-06 17:15:58 +0900

    Add development project

---
+++ Development/Development/DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentApp.swift
+//  Development
+//
+//  Created by shota.todoroki on 2021/12/06.
+//
+
+import SwiftUI
+
+@main
+struct DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
